a03c0ac69d6a01262deb91457063260e79cb2f7b
YoanWithY <yoatc@yahoo.de>
2023-09-24 09:42:09 +0200

    try fix acces problem

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const http = require("http");
-const port = process.env.port || 3000;
+const http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello there!");
-}).listen(port);
+const server = http.createServer(function (request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("<html><body><h1>Hello World!</h1></body></html>");
+});
 
-console.log(`Server is running at http://localhost:${port}`);
+const port = process.env.PORT || 3000;
+server.listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
