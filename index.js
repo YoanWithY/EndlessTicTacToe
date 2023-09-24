9d3a264c7b142239b957be004df1491aaaa5f6c6
YoanWithY <yoatc@yahoo.de>
2023-09-24 22:30:18 +0200

    change import to ecma6 syntax for server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+import * as http from 'http';
 
 const server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/html" });
